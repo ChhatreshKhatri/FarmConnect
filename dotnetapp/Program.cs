bdf22aa7f675cb4c9f17043bfd7a9a268ef86a2a
Chhatresh Khatri <chhatresh.khatri1002@gmail.com>
2025-07-10 16:35:12 +0530

    swagger in prod

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -119,11 +119,17 @@ using (var scope = app.Services.CreateScope())
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FarmConnect API v1");
+    c.RoutePrefix = "swagger"; // Optional: keeps the URL clean at /swagger
+});
 app.UseCors();
 app.UseHttpsRedirection();
 
